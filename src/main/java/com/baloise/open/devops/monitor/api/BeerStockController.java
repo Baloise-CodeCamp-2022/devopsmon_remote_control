84c053af1a2ff68a41e3a89ded23076a22880bdd
Arno Burkhart <arburk@users.noreply.github.com>
2022-10-24 16:05:59 +0200

    added micrometer dependency and logger impl

--- src/main/java/com/baloise/open/devops/monitor/api/BeerStockController.java
+++ src/main/java/com/baloise/open/devops/monitor/api/BeerStockController.java
@@ -1,5 +1,8 @@
 package com.baloise.open.devops.monitor.api;
 
+import com.baloise.open.devops.monitor.util.LogMeterRegistry;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +15,27 @@ import java.util.Random;
 @RequestMapping("v1/beer/stock")
 public class BeerStockController {
 
+  private MeterRegistry meterRegistry;
+  private Counter counter;
+  private Integer gauge;
+
+  public BeerStockController(LogMeterRegistry meterRegistry) {
+    this.meterRegistry = meterRegistry;
+    initMeters();
+  }
+
+  private void initMeters() {
+    this.counter = this.meterRegistry.counter("beer.stock", "beer", "stock");
+    this.gauge = this.meterRegistry.gauge("beer.stock", 0);
+  }
+
+
   @GetMapping(value = "/{make}", produces = "application/json")
   public @ResponseBody int getStockCount(@PathVariable String make) {
-    return new Random().nextInt();
+    this.gauge = new Random().nextInt();
+    this.counter.increment(1.0);
+    System.out.println("getStockCount for " + make);
+    return gauge;
   }
 
 }
--- src/main/java/model/Beer.java
+++
@@ -1,4 +0,0 @@
-package model;
-
-public record Beer(String make) {
-}
