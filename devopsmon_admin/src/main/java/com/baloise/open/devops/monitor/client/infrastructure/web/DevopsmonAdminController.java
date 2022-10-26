e97c3e4db0e6188553c48767871f504501bc56ca
Arno Burkhart <arburk@users.noreply.github.com>
2022-10-26 10:19:08 +0200

    Added some delay into rest endpoint for simualtion purpose

--- devopsmon_admin/src/main/java/com/baloise/open/devops/monitor/client/infrastructure/web/DevopsmonAdminController.java
+++ devopsmon_admin/src/main/java/com/baloise/open/devops/monitor/client/infrastructure/web/DevopsmonAdminController.java
@@ -6,10 +6,14 @@ import io.micrometer.core.annotation.Timed;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RestController
 @RequestMapping("/admin")
 @Slf4j
 public class DevopsmonAdminController implements DevopsmonAdminApi {
+
+    private static AtomicInteger counter = new AtomicInteger(0);
 
     @PostMapping("/create")
     @Timed(value = "greeting.time", description = "Time taken to return greeting",
@@ -20,7 +24,14 @@ public class DevopsmonAdminController implements DevopsmonAdminApi {
     @GetMapping("/blubb")
     @Timed(value = "greeting.time", description = "Time taken to return greeting",
         percentiles = {0.5, 0.90})
-    public void createBlubb(){
+    public void createBlubb() throws InterruptedException {
+        int count = counter.getAndIncrement();
+        if (count > 7 && count < 15) {
+            log.info("delay as count > 7");
+            Thread.sleep(2000 + count * 100);
+        } else {
+            Thread.sleep(1000);
+        }
         log.info("Created event with traceId={} and UUID={}.");
     }
 }
