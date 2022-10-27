ad609e96613d30f7ba63627234c40401fa83d4e7
Arno Burkhart <arburk@users.noreply.github.com>
2022-10-27 12:01:26 +0200

    renamed other modules replacing underscore by dashes

--- devopsmon_controller/src/main/java/com/baloise/open/devops/monitor/controller/domain/service/ActionService.java
+++
@@ -1,7 +0,0 @@
-package com.baloise.open.devops.monitor.controller.domain.service;
-
-import com.baloise.open.devops.monitor.controller.domain.model.RecommendedAction;
-
-public interface ActionService {
-    void proposeRecommendedAction(RecommendedAction action);
-}
--- devopsmon_controller/src/main/java/com/baloise/open/devops/monitor/controller/domain/service/RuleService.java
+++
@@ -1,7 +0,0 @@
-package com.baloise.open.devops.monitor.controller.domain.service;
-
-import com.baloise.open.devops.monitor.controller.domain.model.Event;
-
-public interface RuleService {
-    void processEvent(Event event);
-}
